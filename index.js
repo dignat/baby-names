6915897475eee63f5ba34464ff175ec9a792970f
Denise Ignatova <denis.ignatova@re-flow.co.uk>
2020-09-24 08:29:49 +0100

    handle empty array

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ exports.generateName = (gender, character, length) => {
             genderMatch.push(babyGender);
         }
     }
-    return genderMatch[Math.floor(Math.random()*genderMatch.length)];
+    return genderMatch.length > 0 ? genderMatch[Math.floor(Math.random()*genderMatch.length)] : "No baby name with this criteria";
 }
 
 exports.allNames = allNames;
